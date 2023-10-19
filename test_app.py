4422474613247b8ab9c6848192cca34d47c7bcc3
Syed Faizan Uddin <103924834+faizansyed14@users.noreply.github.com>
2023-10-19 10:52:20 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -10,21 +10,21 @@ def client():
 
 def test_check_sanitization_sanitize(client):
     payload = json.dumps({"input": "This is a safe input"})
-    response = client.post('/input', data=payload, content_type='application/json')
+    response = client.post('/v1/sanitized/input/', data=payload, content_type='application/json')
     data = json.loads(response.data.decode())
     assert response.status_code == 200
     assert data['result'] == "sanitized"
 
 def test_check_sanitization_unsanitize(client):
     payload = json.dumps({"input": "This is an unsafe input DROP TABLE users"})
-    response = client.post('/input', data=payload, content_type='application/json')
+    response = client.post('/v1/sanitized/input/', data=payload, content_type='application/json')
     data = json.loads(response.data.decode())
     assert response.status_code == 200
     assert data['result'] == "unsanitized"
 
 def test_check_sanitization_missing_input(client):
     payload = json.dumps({"data": "This is a test"})
-    response = client.post('/input', data=payload, content_type='application/json')
+    response = client.post('/v1/sanitized/input/', data=payload, content_type='application/json')
     data = json.loads(response.data.decode())
     assert response.status_code == 400
     assert "error" in data
